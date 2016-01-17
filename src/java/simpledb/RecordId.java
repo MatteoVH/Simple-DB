f9ceea077681891f684e135bbd6b603df33ad98b
MatteoVH <matteovh@ucla.edu>
2016-01-16 21:52:45 -0800

    implement lab 1 functionality for RecordId

--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -10,6 +10,9 @@ public class RecordId implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+	private PageId pageId;
+	private int tupleNumber;
+
     /**
      * Creates a new RecordId referring to the specified PageId and tuple
      * number.
@@ -20,23 +23,22 @@ public class RecordId implements Serializable {
      *            the tuple number within the page.
      */
     public RecordId(PageId pid, int tupleno) {
-        // some code goes here
+		this.pageId = pid;
+		this.tupleNumber = tupleno;
     }
 
     /**
      * @return the tuple number this RecordId references.
      */
     public int tupleno() {
-        // some code goes here
-        return 0;
+		return this.tupleNumber;
     }
 
     /**
      * @return the page id this RecordId references.
      */
     public PageId getPageId() {
-        // some code goes here
-        return null;
+		return this.pageId;
     }
 
     /**
@@ -47,8 +49,15 @@ public class RecordId implements Serializable {
      */
     @Override
     public boolean equals(Object o) {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+		if (!(o instanceof RecordId))
+			return false;
+
+		RecordId other = (RecordId)o;
+
+		if (this.tupleno() != other.tupleno() || this.getPageId() != other.getPageId())
+			return false;
+
+		return true;
     }
 
     /**
@@ -59,9 +68,10 @@ public class RecordId implements Serializable {
      */
     @Override
     public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+		String hashStr = "" + this.tupleno() + this.getPageId();
+		int hash = Integer.parseInt(hashStr);
 
+		return hash;
     }
 
 }
