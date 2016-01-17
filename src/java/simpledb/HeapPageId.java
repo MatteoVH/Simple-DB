cbd28397ec4f4ab017d8e863c55490900c485419
MatteoVH <matteovh@ucla.edu>
2016-01-16 21:42:57 -0800

    add Lab 1 HeapPageId functionality

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -3,6 +3,9 @@ package simpledb;
 /** Unique identifier for HeapPage objects. */
 public class HeapPageId implements PageId {
 
+	public int tableId;
+	public int pageNumber;
+
     /**
      * Constructor. Create a page id structure for a specific page of a
      * specific table.
@@ -11,13 +14,13 @@ public class HeapPageId implements PageId {
      * @param pgNo The page number in that table.
      */
     public HeapPageId(int tableId, int pgNo) {
-        // some code goes here
+		this.tableId = tableId;
+		this.pageNumber = pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
-        // some code goes here
-        return 0;
+		return this.tableId;
     }
 
     /**
@@ -25,8 +28,7 @@ public class HeapPageId implements PageId {
      *   this PageId
      */
     public int pageNumber() {
-        // some code goes here
-        return 0;
+		return this.pageNumber;
     }
 
     /**
@@ -36,8 +38,11 @@ public class HeapPageId implements PageId {
      * @see BufferPool
      */
     public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+		String strHash = "" + this.tableId + this.pageNumber;
+
+		int hash = Integer.parseInt(strHash);
+
+		return hash;
     }
 
     /**
@@ -48,8 +53,15 @@ public class HeapPageId implements PageId {
      *   ids are the same)
      */
     public boolean equals(Object o) {
-        // some code goes here
-        return false;
+        if (!(o instanceof PageId))
+			return false;
+
+		PageId other = (PageId)o;
+
+		if (this.getTableId() != other.getTableId() || this.pageNumber() != other.pageNumber())
+			return false;
+
+        return true;
     }
 
     /**
