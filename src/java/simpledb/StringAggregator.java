81efa4b6482d2b8990974fcd0bf8bf66a407efd0
MatteoVH <matteovh@ucla.edu>
2016-02-13 15:48:20 -0800

    implement methods in StringAggregator

--- src/java/simpledb/StringAggregator.java
+++ src/java/simpledb/StringAggregator.java
@@ -1,11 +1,22 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
 public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+	private int gbfield;
+	private Type gbfieldtype;
+	private int afield;
+	private Op what;
+	private int numberOfFieldsAveraged;
+
+	private TupleDesc aggTupleDesc;
+	private ArrayList<Tuple> tuplelist;
 
     /**
      * Aggregate constructor
@@ -17,7 +28,17 @@ public class StringAggregator implements Aggregator {
      */
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+		this.gbfield = gbfield;
+		this.gbfieldtype = gbfieldtype;
+		this.afield = afield;
+		this.what = what;
+
+		this.numberOfFieldsAveraged = 0;
+		
+		tuplelist = new ArrayList<Tuple>();
+
+		if (what != Aggregator.Op.COUNT)
+			throw new IllegalArgumentException();
     }
 
     /**
@@ -25,8 +46,84 @@ public class StringAggregator implements Aggregator {
      * @param tup the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
-    }
+		boolean grouping = false;
+
+		if (gbfield != NO_GROUPING)
+			grouping = true;
+
+		int aggValueIndex = 0;
+
+		if (grouping)
+			aggValueIndex = 1;
+
+		// if we don't have an aggregate tuple yet, establish one
+		if (tuplelist.size() == 0) {
+			TupleDesc aggTupleDesc;
+			Type[] typeArr;
+			String[] fieldArr;
+
+			if (grouping) {
+				//[group by value, aggregate value]
+				typeArr = new Type[] { gbfieldtype, Type.INT_TYPE};
+				fieldArr = new String[] { tup.getTupleDesc().getFieldName(gbfield), what.toString() };
+			} else { //no grouping, so our tuple is just { aggregateVal }
+				typeArr = new Type[] { Type.INT_TYPE};
+				fieldArr = new String[] { what.toString() };
+			}
+
+			this.aggTupleDesc = new TupleDesc( typeArr, fieldArr );
+
+			Tuple aggTuple = new Tuple(this.aggTupleDesc);
+
+			aggTuple.setField(aggValueIndex, new IntField(1));
+
+			if (grouping)
+				aggTuple.setField(0, tup.getField(gbfield));
+
+			tuplelist.add(aggTuple);
+
+		} else { //the list has already been seeded with an aggregate tuple
+
+			Tuple tupleToManipulate = null;
+
+			if (grouping) {
+				boolean tupleFound = false;
+
+				for (Tuple curTuple : tuplelist) {
+					if (tup.getField(gbfield).compare(Predicate.Op.EQUALS, curTuple.getField(0))) {
+						tupleToManipulate = curTuple;
+						tupleFound = true;
+						break;
+					}
+				}
+					
+				//if we did not find a matching group by aggregate we create a new one
+				if (!tupleFound) {
+					Tuple aggTuple = new Tuple(aggTupleDesc);
+
+					aggTuple.setField(aggValueIndex, new IntField(1));
+
+					aggTuple.setField(0, tup.getField(gbfield));
+
+					tuplelist.add(aggTuple);
+
+					return;
+			
+				}
+
+			} else { //we are not grouping, so we'll only have 1 tuple, which is the first
+				tupleToManipulate = tuplelist.get(0);
+			}
+			
+			IntField aggTupAggField = (IntField) tupleToManipulate.getField(aggValueIndex);
+			
+			tupleToManipulate.setField(aggValueIndex, 
+				new IntField(
+					aggTupAggField.getValue() + 1
+				)
+			);
+		}
+	}
 
     /**
      * Create a DbIterator over group aggregate results.
@@ -37,8 +134,10 @@ public class StringAggregator implements Aggregator {
      *   aggregate specified in the constructor.
      */
     public DbIterator iterator() {
-        // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+		return new TupleIterator(
+			this.aggTupleDesc,
+			tuplelist
+		);
     }
 
 }
